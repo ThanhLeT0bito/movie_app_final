8eeb8e0c7699deb8b4b29283f40cc51428d4c682
thaibinh77 <tranbinh772003@gmail.com>
2024-03-15 23:14:14 +0700

    custom bottom navigator

--- lib/screens/home_watching_screen.dart
+++ lib/screens/home_watching_screen.dart
@@ -1,2 +1,33 @@
 import 'package:flutter/material.dart';
 
+import '../resources/app_color.dart';
+import '../widgets/Base/custom_app_bar.dart';
+
+class HomeWatching extends StatefulWidget {
+  const HomeWatching({super.key});
+  static const routeName = '/home-watching';
+
+  @override
+  State<HomeWatching> createState() => _HomeWatchingState();
+}
+
+class _HomeWatchingState extends State<HomeWatching> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: AppColors.BaseColorBlackGround,
+      body: Column(
+        children: [
+          CustomAppBar(
+            title: "Home Watching",
+          ),
+          Expanded(
+            child: Center(
+              child: Text('Hello'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
