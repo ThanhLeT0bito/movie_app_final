1fe0a871d9bf1ec79dbf0721f3cadb63313dd726
dang khoa <vudangkhoa2002345@gmail.com>
2024-04-20 00:09:19 +0700

    Revert "demo demert data actors"
    This reverts commit 19b6e3badb5510c34da55ec3887a6630ff381dc3.

--- lib/providers/movie_providers.dart
+++ lib/providers/movie_providers.dart
@@ -118,7 +118,7 @@ class Movieproviders extends ChangeNotifier {
         thumbnail: "images// nằm dọc",
         category: "category",
         director: "actorID",
-        actor: "6622a046ae30f9d340778130, ",
+        actor: "actorID",
         language: "language",
         publish: "publish",
         censorship: "censorship",
