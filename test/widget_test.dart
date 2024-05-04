2d03e4b61ab0292538b160328c45be55c22e44fe
LeTienThanh <ltthanh22112002@gmail.com>
2024-05-04 16:26:41 +0700

    fix test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:movie_app_final/main.dart';
+import 'package:movie_app_final/models/data_local/UserPreferences%20.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    final preferences = await UserPreferences.getUserId();
+    await tester.pumpWidget(MyApp(preferences: preferences));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
